08e633c860350584b07f7515739423f611d5bfc2
Sebastian Baltser <sebastian.baltser@gmail.com>
2021-10-16 21:52:39 +0200

    Add dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup
 setup(
     name="option-pricing",
     version="0.1.0",
-    author="Sebastian Baltser"
+    author="Sebastian Baltser",
+    install_requires=[
+        "numpy",
+        "scipy"
+    ]
 )
